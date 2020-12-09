aa4737426b53241dbfe9fb4de66600359bda72b4
opilane <opilane@HP-CZC6297B8L>
2020-12-09 11:34:56 +0200

    Something is working now

--- MDPage/MDPage/MainPage.xaml.cs
+++ MDPage/MDPage/MainPage.xaml.cs
@@ -13,6 +13,33 @@ namespace MDPage
         public MainPage()
         {
             InitializeComponent();
+            profileImage.Source = ImageSource.FromFile("marvel.webp");
+            aboutList.ItemsSource = GetMenuList();
+            var homePage = typeof(View.BlackPanther);
+            Detail = new NavigationPage((Page)Activator.CreateInstance(homePage));
+            IsPresented = false;
+        }
+
+        public List<MasterMenuItems> GetMenuList()
+        {
+            var list = new List<MasterMenuItems>();
+            list.Add(new MasterMenuItems()
+            {
+                Text="Железный Человек",
+                Detail="Дебют:1963г.",
+                ImagePath="ironMan.jpg",
+                TargetPage=typeof(View.BlackPanther)
+            });
+            
+            return list;
+        }
+
+        private void aboutList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var selectedMenuItem = (MasterMenuItems)e.SelectedItem;
+            Type selectedPage = selectedMenuItem.TargetPage;
+            Detail = new NavigationPage((Page)Activator.CreateInstance(selectedPage));
+            IsPresented = false;
         }
     }
 }
--- MDPage/MDPage/MasterMenuItems.cs
+++ MDPage/MDPage/MasterMenuItems.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿    using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MDPage
 {
-    class MasterMenuItems
+    public class MasterMenuItems
     {
         public string Text { get; set; }
         public string Detail { get; set; }
